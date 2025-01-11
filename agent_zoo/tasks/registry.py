86209ac17941af3fc31b4d5e5c86ffb9cd6ac51b
Benedikt Stroebl <stroebl@princeton.edu>
2025-01-11 21:23:35 +0000

    added humor task

--- agent_zoo/tasks/registry.py
+++ agent_zoo/tasks/registry.py
@@ -5,11 +5,25 @@ class BasicTask(Task):
         super().__init__(
             name="basic_task",
             evaluation_function=self.evaluate,
-            prompt="Check your mail",
+            prompt="Send a message to the other agent",
             environment_vars={}
         )
     
     def evaluate(self, response: str) -> bool:
         return response.lower() == "paris"
     
+    
+class HumorTask(Task):
+    def __init__(self):
+        super().__init__(
+            name="generate_jokes",
+            evaluation_function=self.evaluate,
+            prompt="You should work on writing the most humorous joke that the world has ever seen. You should keep iterating on it indefinitely.",
+            environment_vars={}
+        )
+    
+    def evaluate(self, response: str) -> bool:
+        pass
+    
 basic_task = BasicTask()
+humor_task = HumorTask()
