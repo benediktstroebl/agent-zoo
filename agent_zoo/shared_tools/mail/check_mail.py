02c5c07eb18cec71e32d90a51219b147ebd163dc
Veniamin Veselovsky <venia@veselovsky.org>
2025-01-11 16:32:23 -0500

    addding mail tools

--- agent_zoo/shared_tools/mail/check_mail.py
+++ agent_zoo/shared_tools/mail/check_mail.py
@@ -6,8 +6,8 @@ def check_mail():
     """
     workspace_dir = os.getenv('WORKSPACE_DIR')
     agent_name = os.getenv('AGENT_NAME')
-    mail_dir = os.getenv('MAIL_DIR')
-    mail_path = os.path.join(workspace_dir, mail_dir, 'mail')
+    mail_dir = os.getenv('MAIL_DIRECTORY')
+    mail_path = os.path.join(workspace_dir, agent_name, mail_dir, 'mail')
     with open(mail_path, 'r') as f:
         mail = f.read()
     return mail 
