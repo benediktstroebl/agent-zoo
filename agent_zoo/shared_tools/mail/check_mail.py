333827d90f406c2e5a42617b100a4afd47a9f621
Benedikt Stroebl <stroebl@princeton.edu>
2025-01-11 22:48:12 +0000

    update and error fix

--- agent_zoo/shared_tools/mail/check_mail.py
+++ agent_zoo/shared_tools/mail/check_mail.py
@@ -1,13 +1,12 @@
-import os 
-
 def check_mail(): 
     """
     check the mail for the agent. 
     """
-    workspace_dir = os.getenv('WORKSPACE_DIR')
-    agent_name = os.getenv('AGENT_NAME')
-    mail_dir = os.getenv('MAIL_DIRECTORY')
-    mail_path = os.path.join(workspace_dir, agent_name, mail_dir, 'mail')
-    with open(mail_path, 'r') as f:
+    import os 
+    workspace_dir = os.getenv("WORKSPACE_DIR")
+    agent_name = os.getenv("AGENT_NAME")
+    mail_dir = os.getenv("MAIL_DIRECTORY")
+    mail_path = os.path.join(workspace_dir, agent_name, mail_dir, "mail")
+    with open(mail_path, "r") as f:
         mail = f.read()
     return mail 
--- agent_zoo/shared_tools/mail/send_message.py
+++ agent_zoo/shared_tools/mail/send_message.py
@@ -1,29 +1,25 @@
-import os 
 
-from logging import getLogger
 
-logger = getLogger(__name__)
 
-msg_template = """
+def send_message(recipient_name, msg): 
+    """
+    send a message to the agent. 
+    """
+    msg_template = """
 =========================
 Sender: {sender_name}
 
 Message: {msg}
 
 """
+    import os 
+    sender_name = os.getenv("AGENT_NAME")
+    workspace_dir = os.getenv("WORKSPACE_DIR")
+    mail_dir = os.getenv("MAIL_DIRECTORY")
 
-def send_message(recipient_name, msg): 
-    """
-    send a message to the agent. 
-    """
-    logger.info(f"Sending message to {recipient_name}")
-    sender_name = os.getenv('AGENT_NAME')
-    workspace_dir = os.getenv('WORKSPACE_DIR')
-    mail_dir = os.getenv('MAIL_DIRECTORY')
-
-    mail_path = os.path.join(workspace_dir, recipient_name, mail_dir, 'mail')
-    logger.info(f"Mail path: {mail_path}")
-    with open(mail_path, 'a') as f:
+    mail_path = os.path.join(workspace_dir, recipient_name, mail_dir, "mail")
+    
+    with open(mail_path, "a") as f:
         msg_template = msg_template.format(sender_name=sender_name, msg=msg)
         f.write(msg_template)
     return mail_path
