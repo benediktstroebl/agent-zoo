012989924f224e6c820e71bb89328a5f61f25c0a
Benedikt Stroebl <stroebl@princeton.edu>
2025-01-12 00:31:13 +0000

    update

--- agent_zoo/tasks/registry.py
+++ agent_zoo/tasks/registry.py
@@ -5,7 +5,7 @@ class BasicTask(Task):
         super().__init__(
             name="basic_task",
             evaluation_function=self.evaluate,
-            prompt="Send a message to the other agent",
+            prompt="Ask human for help",
             environment_vars={}
         )
     
