4e8099f1ae045adf42c1bdcb38fc5210a2f0ecc6
Benedikt Stroebl <stroebl@princeton.edu>
2025-02-01 21:24:06 +0000

    merge

--- agent_zoo/shared_tools/__init__.py
+++ agent_zoo/shared_tools/__init__.py
@@ -1,5 +1,6 @@
 from .mail.mail import Mail
 from .wait.wait_class import Wait
 from .blog.blog import Blog
-from .evaluate_agent.evaluate import EvaluateUSACO
+from .evaluate_agent.usaco import EvaluateUSACO
+from .evaluate_agent.zygosity import EvaluateZygosity
 from .slack import Slack
