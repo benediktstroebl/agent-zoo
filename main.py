8c9977d9525fed7e136ee9e29f21f28c8023a6a0
Benedikt Stroebl <stroebl@princeton.edu>
2025-02-01 21:19:16 +0000

    update

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from agent_zoo.configs.compute_config import DockerComputeConfig
 from agent_zoo.configs.permissions_config import PermissionsConfig
 from pathlib import Path
 from dotenv import load_dotenv
-from agent_zoo.shared_tools.ask_human.slack import Slack
+from agent_zoo.shared_tools.slack.slack import Slack
 
 load_dotenv()
 
 from agent_zoo.shared_tools.mail.mail import Mail
 from agent_zoo.shared_tools.wait.wait_class import Wait
 from agent_zoo.shared_tools.blog.blog import Blog
-from agent_zoo.shared_tools.evaluate_agent.evaluate import EvaluateUSACO
+from agent_zoo.shared_tools.evaluate_agent.usaco import EvaluateUSACO
+from agent_zoo.shared_tools.evaluate_agent.zygosity import EvaluateZygosity
+
 def print_ascii_art():
     try:
         with open('ascii_load_image.txt', 'r') as f:
@@ -23,16 +25,16 @@ def main():
     
     zoo = AgentZoo(
         name="math_2_agents_20_minutes_4o",
-        agents=["monkey", "giraffe"],
-        tasks=['866_platinum_the_cow_gathering'],
+        agents=["monkey", "hawk"],
+        tasks=['twindat_sim_1k_24'],
         compute_config=DockerComputeConfig(cpu_cores=2, memory_limit="4g", gpu_devices=[0], shared_memory_size="1g", network_mode="bridge"),
         permissions_config=PermissionsConfig(cpu_cores=2, memory_limit="4g", gpu_devices=[0], shared_memory_size="1g", network_mode="bridge"),
         shared_tools=[
             Blog(), 
             Slack(world_agent_mapping=
                   {
-        "w1": ['monkey', 'giraffe']}),
-            EvaluateUSACO()
+        "w1": ['monkey', 'hawk']}),
+            EvaluateZygosity()
         ],
         workspace_config_path=Path('agent_zoo/configs/default_workspace.yaml'),
         max_runtime_minutes=60    
